0810c19d85d2a23b74899e77a5188380eea2c456
MurrayCode <mhstewart1994@gmail.com>
2024-12-03 20:55:49 +0000

    feat: day3 P2 & refactor P1

--- puzzles/day3/main.go
+++ puzzles/day3/main.go
@@ -5,13 +5,36 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 func main() {
 	read, _ := os.ReadFile("input.txt")
 	fmt.Println(Part1(string(read)))
+	fmt.Println(Part2(string(read)))
+}
+
+func Part2(input string) int {
+	reader := strings.NewReader(input)
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+	res := 0
+	do := true
+	for scanner.Scan() {
+		r := regexp.MustCompile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
+		for _, v := range r.FindAllString(scanner.Text(), -1) {
+			if v[0:3] == "mul" {
+				var val1, val2 int
+				fmt.Sscanf(v, "mul(%d,%d)", &val1, &val2)
+				if do {
+					res += val1 * val2
+				}
+			} else {
+				do = v == "do()"
+			}
+		}
+	}
+	return res
 }
 
 func Part1(input string) int {
@@ -21,10 +44,10 @@ func Part1(input string) int {
 	res := 0
 	for scanner.Scan() {
 		r := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-		matches := r.FindAllStringSubmatch(scanner.Text(), -1)
+		matches := r.FindAllString(scanner.Text(), -1)
 		for _, v := range matches {
-			val1, _ := strconv.Atoi(v[1])
-			val2, _ := strconv.Atoi(v[2])
+			var val1, val2 int
+			fmt.Sscanf(v, "mul(%d,%d)", &val1, &val2)
 			res += val1 * val2
 		}
 	}
--- puzzles/day3/main_test.go
+++ puzzles/day3/main_test.go
@@ -12,3 +12,11 @@ func TestPart1(t *testing.T) {
 		t.Errorf("Got %d, expected %d", res, exp)
 	}
 }
+func TestPart2(t *testing.T) {
+	input := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()?mul(2,4)do()+mul(2,2)`
+	exp := 52
+	res := Part2(input)
+	if res != exp {
+		t.Errorf("Got %d, expected %d", res, exp)
+	}
+}
