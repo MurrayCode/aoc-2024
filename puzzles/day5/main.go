19a988a24b99743e6676ba6a56faf259f6528dbf
MurrayCode <mhstewart1994@gmail.com>
2024-12-07 14:32:41 +0000

    feat: day5 P2

--- puzzles/day5/main.go
+++ puzzles/day5/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 
 	s "github.com/murraycode/aoc-2024/pkg/slices"
@@ -12,6 +14,7 @@ import (
 func main() {
 	read, _ := os.ReadFile("input.txt")
 	fmt.Println(Part1(string(read)))
+	fmt.Println(Part2(string(read)))
 }
 
 func getRules(input string) []string {
@@ -65,6 +68,70 @@ func Part1(input string) int {
 		}
 		if ok {
 			res += s.GetMiddleValueConvertToInt(strings.Split(update, ","))
+		}
+	}
+	return res
+}
+
+func convertSliceStringToInts(arr []string) []int {
+	res := make([]int, len(arr))
+	for i, item := range arr {
+		res[i], _ = strconv.Atoi(item)
+	}
+	return res
+}
+
+func Part2(input string) int {
+	reader := strings.NewReader(input)
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+	var rules [][]string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		rules = append(rules, getRules(line))
+	}
+	updates := strings.Split(input, "\n\n")[1]
+	arrUpdates := strings.Split(updates, "\n")
+	res := 0
+	brokenUpdates := [][]int{}
+	for _, update := range arrUpdates {
+		ok := true
+		for _, rule := range rules {
+			ok = checkRule(rule, update)
+			if !ok {
+				break
+			}
+		}
+		if !ok {
+			bu := strings.Split(update, ",")
+			brokenUpdates = append(brokenUpdates, convertSliceStringToInts(bu))
+		}
+
+		var compare = func(l, r int) int {
+			for _, rule := range rules {
+
+				one, _ := strconv.Atoi(rule[0])
+				two, _ := strconv.Atoi(rule[1])
+
+				if one == r && two == l {
+					return 1
+				}
+				if one == l && two == r {
+					return -1
+				}
+			}
+			return 0
+		}
+		for _, bu := range brokenUpdates {
+			if slices.IsSortedFunc(bu, compare) {
+				continue
+			} else {
+				slices.SortFunc(bu, compare)
+				res += s.GetMiddleValue(bu)
+			}
 		}
 	}
 	return res
