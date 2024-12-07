ce3839c58034956aa675459d9e13d73d59b423db
MurrayCode <mhstewart1994@gmail.com>
2024-12-07 14:33:08 +0000

    feat:moved helpers into pkg

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -1,5 +1,24 @@
 package slices
 
+import "strconv"
+
 func DeleteElement(slice []int, index int) []int {
 	return append(slice[:index], slice[index+1:]...)
+}
+func Contains(arr []string, item string) bool {
+	for _, i := range arr {
+		if i == item {
+			return true
+		}
+	}
+	return false
+}
+func GetMiddleValueConvertToInt(arr []string) int {
+	m := len(arr) / 2
+	i, _ := strconv.Atoi(arr[m])
+	return i
+}
+func GetMiddleValue(arr []int) int {
+	m := len(arr) / 2
+	return arr[m]
 }
